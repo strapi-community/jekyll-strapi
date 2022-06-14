78eac2c1329110c686cb307485e35f24d81e68ce
Rafał Zawadzki <bluszcz@bluszcz.net>
2022-06-15 00:39:47 +0200

    Better logic for HTTP requests

--- lib/jekyll/strapi/strapihttp.rb
+++ lib/jekyll/strapi/strapihttp.rb
@@ -2,16 +2,19 @@
 # This is a helper method to authenticate during getting data from Strapi instance.
 
 def strapi_request(url)
-    strapi_token = ENV['STRAPI_TOKEN']
     uri = URI(url)
     req = Net::HTTP::Get.new(uri)
-    req['Authorization'] = "Bearer #{strapi_token}"
-    headers = {
-        'Authorization'=>"Bearer #{strapi_token}"
-    }
 
+    strapi_token = ENV['STRAPI_TOKEN']
     if strapi_token==nil
         Jekyll.logger.info "STRAPI_TOKEN not set, non Authenticated request."
+        headers = {}
+    else
+        headers = {
+            'Authorization'=>"Bearer #{strapi_token}"
+        }
+        req['Authorization'] = "Bearer #{strapi_token}"
+
     end
     Jekyll.logger.info "Jekyll StrapiHttp:", "Fetching entries from #{uri} using headers: #{headers.keys}"
     Net::HTTP.get_response(uri, headers)
